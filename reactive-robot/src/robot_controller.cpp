78f9262fc326bda2faf3d6673b2e644ca10f2a13
Rekicho <up201604145@fe.up.pt>
2020-11-09 16:06:11 +0000

    robot turns 90º to wall

--- reactive-robot/src/robot_controller.cpp
+++ reactive-robot/src/robot_controller.cpp
@@ -1,16 +1,37 @@
 #include <ros/ros.h>
 
 #include <sensor_msgs/LaserScan.h>
-#include <std_msgs/Int32.h>
-
 #include <geometry_msgs/Twist.h>
+
+#include <limits>
+#include <cmath>
+
+#define INF std::numeric_limits<float>::max()
+#define PI 2*acos(0.0)
 
 ros::Publisher vel_pub;
 
 void clbk_laser(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
+    float min_range = INF;
+    float min_angle = INF;
+
+
+    for(int i = 0; i < msg->ranges.size(); ++i)
+    {
+        float angle = msg->angle_min + i * msg->angle_increment;
+
+        if(msg->ranges[i] < min_range)
+        {
+            min_range = msg->ranges[i];
+            min_angle = angle;
+        }
+    }
+
+    ROS_INFO("min_range: %f, min_angle: %f", min_range, min_angle);
+
     geometry_msgs::Twist res_msg;
-    res_msg.linear.x = 1;
+    res_msg.angular.z = min_angle - (PI / 2);
     vel_pub.publish(res_msg);
 }
 
