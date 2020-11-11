37f6ae4ded787d71b1740e4b57ae981d78de12d2
Rekicho <up201604145@fe.up.pt>
2020-11-11 19:52:13 +0000

    Not cornering

--- reactive-robot/src/robot_controller.cpp
+++ reactive-robot/src/robot_controller.cpp
@@ -5,12 +5,18 @@
 
 #include <limits>
 #include <cmath>
+#include <vector>
+#include <utility>
+
 
 #define INF std::numeric_limits<float>::max()
 #define PI (2*acos(0.0))
 
-#define FORWARD_VELOCITY 0.2
+#define FORWARD_VELOCITY 0.3
 #define ANGULAR_VELOCITY 3
+#define ANG_VEL_FAR_AWAY 0.4
+#define ANG_VEL_TOO_CLOSE 0.25
+#define NUMBER_IMP_LASERS 100
 
 ros::Publisher vel_pub;
 
@@ -24,47 +30,86 @@ float degree_to_radian(float degrees) {
 
 void clbk_laser(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
+    geometry_msgs::Twist res_msg;
     float min_range = INF;
     float min_angle = INF;
+    std::vector<std::pair<float, float>> dist_ang;
 
     for(int i = 0; i < msg->ranges.size(); ++i)
     {
-        float angle = radian_to_degree(msg->angle_min + (i * msg->angle_increment));
+        float angle_rad = msg->angle_min + (i * msg->angle_increment);
+        float angle = radian_to_degree(angle_rad);
 
-        if(msg->ranges[i] < min_range)
+        dist_ang.push_back(std::make_pair(msg->ranges[i],angle_rad));
+
+        if((angle < 180 && msg->ranges[i] < min_range) || (angle >= 180 && msg->ranges[i] <= min_range))
         {
             min_range = msg->ranges[i];
             min_angle = angle;
         }
     }
 
-    ROS_INFO("min_range: %f m, min_angle: %f o", min_range, min_angle);
+    sort(dist_ang.begin(), dist_ang.end());
 
-    geometry_msgs::Twist res_msg;
+    float x = 0;
+    float y = 0;
 
-    if(min_range >= 0.2)
+    for(int i = 0; i < NUMBER_IMP_LASERS; ++i)
     {
-        res_msg.linear.x = FORWARD_VELOCITY;
+        float weight = 1 / pow(dist_ang[i].first,5);
+        x += cos(dist_ang[i].second) * weight;
+        y += sin(dist_ang[i].second) * weight;
+    }   
+
+    float mean = radian_to_degree(atan2(y,x));
+
+    if(mean < 0)
+        mean += 360;
+
+    min_angle = mean;
+
+    ROS_INFO("min_range: %f m, normal_angle: %f o", min_range, min_angle);
+    res_msg.linear.x = FORWARD_VELOCITY;
+
+    if(min_range <= 0.1205)
+    {
+        ROS_INFO("Perto");
 
         if(min_angle > 180)
         {
-            float turn_angle = degree_to_radian(abs(min_angle - 360));
-            res_msg.angular.z = -turn_angle;
+            float turn_angle = 1.5 * degree_to_radian(abs(min_angle - 360));
+            res_msg.angular.z = turn_angle * ANG_VEL_TOO_CLOSE;
         }
 
         else
         {
-            float turn_angle = degree_to_radian(min_angle);
-            res_msg.angular.z = turn_angle;
+            float turn_angle = 1.5 * degree_to_radian(min_angle);
+            res_msg.angular.z = -turn_angle * ANG_VEL_TOO_CLOSE;
+        }
+    }
+
+    else if(min_range >= 0.225)
+    {
+        ROS_INFO("Longe");
+
+        if(min_angle > 180)
+        {
+            float turn_angle = 1.5 * degree_to_radian(abs(min_angle - 360));
+            res_msg.angular.z = -turn_angle * ANG_VEL_FAR_AWAY;
+        }
+
+        else
+        {
+            float turn_angle = 1.5 * degree_to_radian(min_angle);
+            res_msg.angular.z = turn_angle * ANG_VEL_FAR_AWAY;
         }
     }
 
     else if(abs(min_angle - 90) <= abs(min_angle - 270))
     {
-        float turn_angle = degree_to_radian(abs(min_angle - 90));
-        res_msg.linear.x = FORWARD_VELOCITY * cos(turn_angle);
+        float turn_angle = 1.5 * degree_to_radian(abs(min_angle - 90));
 
-        ROS_INFO("Parede Direita: Angle %f", turn_angle);
+        ROS_INFO("Parede Esquerda: Angle %f", turn_angle);
 
         if (min_angle > 90)
             res_msg.angular.z = ANGULAR_VELOCITY * turn_angle;
@@ -74,10 +119,9 @@ void clbk_laser(const sensor_msgs::LaserScan::ConstPtr& msg)
 
     else
     {
-        float turn_angle = degree_to_radian(abs(min_angle - 270));
-        res_msg.linear.x = FORWARD_VELOCITY * cos(turn_angle);
+        float turn_angle = 1.5 * degree_to_radian(abs(min_angle - 270));
 
-        ROS_INFO("Parede Esquerda: Angle %f", turn_angle);
+        ROS_INFO("Parede Direita: Angle %f", turn_angle);
 
         if (min_angle > 270)
             res_msg.angular.z = ANGULAR_VELOCITY * turn_angle;
@@ -85,10 +129,7 @@ void clbk_laser(const sensor_msgs::LaserScan::ConstPtr& msg)
         else res_msg.angular.z = ANGULAR_VELOCITY * -turn_angle;
     }
 
-    if (min_range <= 0.15 && (min_angle < 10 || min_angle > 350))
-    {
-        res_msg.linear.x = -FORWARD_VELOCITY;
-    }
+    ROS_INFO("Vel lin: %f ang: %f", res_msg.linear.x, res_msg.angular.z);
 
     vel_pub.publish(res_msg);
 }
